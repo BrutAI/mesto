a8ba70af2cfd285dc2de118fa7183cdfe7c5e19f
Andrey <andrey@booqva.com>
2021-03-27 19:56:45 +0300

    убрал старый код

--- scripts/script.js
+++ scripts/script.js
@@ -92,42 +92,5 @@ document.querySelector('.profile__edit-button').addEventListener('click', functi
   toggleModal(user);
 });
 
-
-// let editButton = document.querySelector('.profile__edit-button');
-// let nameProfile = document.querySelector('.profile__name');
-// let jobProfile = document.querySelector('.profile__job');
-
-// let popup = document.querySelector('.popup');
-// let closeButton = document.querySelector('.popup__close');
-
-// let formElement = document.querySelector('.popup__form');
-// let nameInput = formElement.querySelector('.popup__input_value_name');
-// let jobInput = formElement.querySelector('.popup__input_value_job');
-
-// function openModalProfile() {
-//   nameInput.value = nameProfile.textContent;
-//   jobInput.value = jobProfile.textContent;
-//   popup.classList.add('popup_opened');
-//   root.classList.add('root_modal');
-// }
-
-
-
-// function formSubmitHandler(evt) {
-//   evt.preventDefault();
-//   nameProfile.textContent = nameInput.value;
-//   jobProfile.textContent = jobInput.value;
-//   popup.classList.remove('popup_opened');
-//   root.classList.remove('root_modal');
-// }
-
-// при загрузке
-// добавляем все карточки на страницу
 initialCards.forEach(addItem);
 
-// устанавливаем обработчики
-// formElement.addEventListener('submit', formSubmitHandler);
-//document.querySelector('.profile__edit-button').addEventListener('click', toggleModal(document.querySelector('.profile')));
-
-//editButton.addEventListener('click', toggleModal(add));
-//closeButton.addEventListener('click', closeModal);
