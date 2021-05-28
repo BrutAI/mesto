83a3f2fa992d1ee3d3aed18a75e6a0a6c1334edb
Andrey <andrey@booqva.com>
2021-05-28 14:37:43 +0300

    подготовил все по заданию и проверил

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import '../pages/index.css';
+import './index.css';
 
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
-import PopupWithForm from './PopupWithForm.js';
-import PopupWithImage from './PopupWithImage.js';
-import UserInfo from './UserInfo.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import UserInfo from '../components/UserInfo.js';
 
 const initialCards = [
   {
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {main: './src/scripts/index.js'},
+  entry: {main: './src/pages/index.js'},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
     compress: true,
-    port: 8081,
+    port: 8082,
     open: true
   },
   module: {
